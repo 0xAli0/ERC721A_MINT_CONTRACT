be659c828aa2a174548b5f6eb160ba75a005fab7
OXI <52327373+oxi-eth@users.noreply.github.com>
2022-04-07 23:14:36 +0300

    Add files via upload

---
+++ Metavirtualcity.sol
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MetaVirtualCity is ERC721A, Ownable{
+    using Strings for uint256;
+
+    uint256 public constant MAX_SUPPLY = 6665;
+    uint256 public constant MAX_PUBLIC_MINT = 10;
+    uint256 public constant MAX_WHITELIST_MINT = 3;
+    uint256 public constant PUBLIC_SALE_PRICE = .099 ether;
+    uint256 public constant WHITELIST_SALE_PRICE = .0799 ether;
+
+    string private  baseTokenUri;
+    string public   placeholderTokenUri;
+
+    //deploy smart contract, toggle WL, toggle WL when done, toggle publicSale 
+    //2 days later toggle reveal
+    bool public isRevealed;
+    bool public publicSale;
+    bool public whiteListSale;
+    bool public pause;
+    bool public teamMinted;
+
+    bytes32 private merkleRoot;
+
+    mapping(address => uint256) public totalPublicMint;
+    mapping(address => uint256) public totalWhitelistMint;
+
+    constructor() ERC721A("Meta Virtual City", "MVC"){
+
+    }
+
+    modifier callerIsUser() {
+        require(tx.origin == msg.sender, "Meta Virtual City :: Cannot be called by a contract");
+        _;
+    }
+
+    function mint(uint256 _quantity) external payable callerIsUser{
+        require(publicSale, "Meta Virtual City :: Not Yet Active.");
+        require((totalSupply() + _quantity) <= MAX_SUPPLY, "Meta Virtual City :: Beyond Max Supply");
+        require((totalPublicMint[msg.sender] +_quantity) <= MAX_PUBLIC_MINT, "Meta Virtual City :: Already minted 3 times!");
+        require(msg.value >= (PUBLIC_SALE_PRICE * _quantity), "Meta Virtual City :: Below ");
+
+        totalPublicMint[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function whitelistMint(bytes32[] memory _merkleProof, uint256 _quantity) external payable callerIsUser{
+        require(whiteListSale, "Meta Virtual City :: Minting is on Pause");
+        require((totalSupply() + _quantity) <= MAX_SUPPLY, "Meta Virtual City :: Cannot mint beyond max supply");
+        require((totalWhitelistMint[msg.sender] + _quantity)  <= MAX_WHITELIST_MINT, "Meta Virtual City :: Cannot mint beyond whitelist max mint!");
+        require(msg.value >= (WHITELIST_SALE_PRICE * _quantity), "Meta Virtual City :: Payment is below the price");
+        //create leaf node
+        bytes32 sender = keccak256(abi.encodePacked(msg.sender));
+        require(MerkleProof.verify(_merkleProof, merkleRoot, sender), "Meta Virtual City :: You are not whitelisted");
+
+        totalWhitelistMint[msg.sender] += _quantity;
+        _safeMint(msg.sender, _quantity);
+    }
+
+    function teamMint() external onlyOwner{
+        require(!teamMinted, "Meta Virtual City :: Team already minted");
+        teamMinted = true;
+        _safeMint(msg.sender, 200);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenUri;
+    }
+
+    //return uri for certain token
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        uint256 trueId = tokenId + 1;
+
+        if(!isRevealed){
+            return placeholderTokenUri;
+        }
+        //string memory baseURI = _baseURI();
+        return bytes(baseTokenUri).length > 0 ? string(abi.encodePacked(baseTokenUri, trueId.toString(), ".json")) : "";
+    }
+
+    /// @dev walletOf() function shouldn't be called on-chain due to gas consumption
+    function walletOf() external view returns(uint256[] memory){
+        address _owner = msg.sender;
+        uint256 numberOfOwnedNFT = balanceOf(_owner);
+        uint256[] memory ownerIds = new uint256[](numberOfOwnedNFT);
+
+        for(uint256 index = 0; index < numberOfOwnedNFT; index++){
+            ownerIds[index] = tokenOfOwnerByIndex(_owner, index);
+        }
+
+        return ownerIds;
+    }
+
+    function setTokenUri(string memory _baseTokenUri) external onlyOwner{
+        baseTokenUri = _baseTokenUri;
+    }
+    function setPlaceHolderUri(string memory _placeholderTokenUri) external onlyOwner{
+        placeholderTokenUri = _placeholderTokenUri;
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner{
+        merkleRoot = _merkleRoot;
+    }
+
+    function getMerkleRoot() external view returns (bytes32){
+        return merkleRoot;
+    }
+
+    function togglePause() external onlyOwner{
+        pause = !pause;
+    }
+
+    function toggleWhiteListSale() external onlyOwner{
+        whiteListSale = !whiteListSale;
+    }
+
+    function togglePublicSale() external onlyOwner{
+        publicSale = !publicSale;
+    }
+
+    function toggleReveal() external onlyOwner{
+        isRevealed = !isRevealed;
+    }
+
+    function withdraw() external onlyOwner{
+        //35% to utility/investors wallet
+        uint256 withdrawAmount_35 = address(this).balance * 35/100;
+        //20% to artist (post utility)
+        uint256 withdrawAmount_20 = (address(this).balance - withdrawAmount_35) * 20/100;
+        //5% to advisor (post utility)
+        uint256 withdrawAmount_5 = (address(this).balance - withdrawAmount_35) * 5/100;
+        payable(0x577C085D873e6197D3ce26dE7A4a1050085864b6).transfer(withdrawAmount_35);
+        payable(0xa6b3E06523227F04E0Cbe62AAbB239f6BD889408).transfer(withdrawAmount_20);
+        payable(0x83C6A04f5e54A1F2730241FB0C93857A7B011Aa4).transfer(withdrawAmount_5);
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
